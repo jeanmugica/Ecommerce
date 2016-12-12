266fd428d08c1eede3f76eea439224f986316516
Jean Carlo Faria Mugica <jeanmugica@gmail.com>
2016-12-12 18:39:42 -0200

    primeiro commit

---
+++ CorujasMimo.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CorujasMimo.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestCorujasMimo
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ CorujasMimo.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CorujasMimo.LojaVirtual.Web.MvcApplication" Language="C#" %>
