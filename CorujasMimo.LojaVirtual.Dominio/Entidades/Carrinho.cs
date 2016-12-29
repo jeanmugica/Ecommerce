ddf71b36e89f1009565fef8f1b9a92a2eb96d700
Jean Carlo Faria Mugica <jeanmugica@gmail.com>
2016-12-29 13:34:48 -0200

    aula 15 Carrinho de Compras

---
+++ CorujasMimo.LojaVirtual.Dominio/Entidades/Carrinho.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CorujasMimo.LojaVirtual.Dominio.Entidades
+{
+    public class Carrinho
+    {
+        private readonly List<ItemCarrinho> _itemCarrinho = new List<ItemCarrinho>();
+        //Adicionar
+        public void AdicionarItem(Produtos produtos, int quantidade)
+        {
+            ItemCarrinho item = _itemCarrinho.FirstOrDefault(p => p.Produtos.ProdutoId == produtos.ProdutoId);
+
+            if (item == null)
+            {
+                _itemCarrinho.Add(new ItemCarrinho
+                {
+                    Produtos = produtos,
+                    Quantidade = quantidade
+                });
+            }
+        }
+        
+        //Remover item do carrinho
+        public void RemoverItem(Produtos produtos)
+        {
+            _itemCarrinho.RemoveAll(l => l.Produtos.ProdutoId == produtos.ProdutoId);
+        }
+
+        //Limpar carrinho
+        public void LimparCarrinho(Produtos produtos)
+        {
+            _itemCarrinho.Clear();
+        }
+        
+        //Obrter o valor total
+        public decimal ObterValorTotal()
+        {
+            return _itemCarrinho.Sum(e => e.Produtos.Preco * e.Quantidade);
+        }
+
+        //Itens do carrinho
+        public IEnumerable<ItemCarrinho> ItensCarrinho
+        {
+            get {return _itemCarrinho; }
+        }
+    }
+}
+
---
+++ CorujasMimo.LojaVirtual.Dominio/Entidades/ItemCarrinho.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorujasMimo.LojaVirtual.Dominio.Entidades
+{
+    public class ItemCarrinho
+    {
+        public Produtos Produtos { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
